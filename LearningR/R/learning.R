b9f42c91e5ac41a8630070dca71f798256be9725
jlauszus <johanneslothlauszus@gmail.com>
2024-01-24 12:28:17 +0100

    Learning how to sort data + exercises

--- LearningR/R/learning.R
+++ LearningR/R/learning.R
@@ -48,5 +48,96 @@ NHANES_small %>%
   select(bmi, contains("age"))
 
 NHANES_small %>%
+    blood_pressure <- select(starts_with("bp_"))
+
+blood_pressure %>%
+    rename(bp_systolic = bp_sys_ave)
+
+NHANES_small %>%
   select(starts_with("bp_")) %>%
   rename(bp_systolic = bp_sys_ave)
+
+# Filtering data by row ---------------------------------------------------
+
+filter(NHANES_small,phys_active == "No")
+NHANES_small %>%
+    filter(phys_active == "No")
+NHANES_small %>%
+    filter(phys_active != "No")
+
+NHANES_small %>%
+    filter(bmi == 25)
+NHANES_small %>%
+    filter(bmi >= 25)
+
+NHANES_small %>%
+    filter(sex == "male")
+
+TRUE & TRUE
+TRUE & FALSE
+FALSE & FALSE
+TRUE | TRUE
+TRUE | FALSE
+FALSE | FALSE
+
+NHANES_small %>%
+    filter(bmi == 25 & phys_active == "No") %>%
+    select(bmi, phys_active)
+
+NHANES_small %>%
+    filter(bmi == 25 | phys_active == "No") %>%
+    select(bmi, phys_active)
+
+# Arranging the rows ------------------------------------------------------
+NHANES_small %>%
+    arrange(age)
+NHANES_small %>%
+    arrange(education) %>%
+    select(education)
+NHANES_small %>%
+    arrange(desc(age)) %>%
+    select(age)
+NHANES_small %>%
+    arrange(age, education) %>%
+    select (age, education)
+
+# Transform or add columns ------------------------------------------------
+# Transform
+NHANES_small %>%
+    mutate(age = age * 12)
+NHANES_small %>%
+    mutate(age = age * 12 ,
+           logged_bmi = log(bmi)) %>%
+    select(age, logged_bmi)
+
+# add new column
+NHANES_small %>%
+    mutate(
+        old = if_else(age >= 30, "Yes", "No")
+    ) %>%
+    select(old)
+
+# Exercise 7.12 -----------------------------------------------------------
+# 1. BMI between 20 and 40 with diabetes
+NHANES_small %>%
+    # Format should follow: variable >= number or character
+    filter(bmi >= 20 & bmi <= 40 & diabetes == "Yes" )
+
+# Pipe the data into mutate function and:
+nhanes_modified <- nhanes_small %>% # Specifying dataset
+nhanes_modified <- NHANES_small
+
+# 2. Calculate mean arterial pressure
+nhanes_modified %>%
+    mutate(
+        mean_arteial_pressure = ((2 * bp_dia_ave) + bp_sys_ave) / 3
+    ) %>%
+    select(mean_arteial_pressure)
+
+# 3. Create young_child variable using a condition
+nhanes_modified %>%
+    mutate(
+        young_child = if_else(age < 6, "Yes", "No")) %>%
+    select(young_child)
+
+
